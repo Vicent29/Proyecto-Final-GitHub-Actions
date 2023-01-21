da355b4ca994f11cd2b29cc2a66b9691c8cfe847
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2023-01-21 03:21:25 +0100

    notification_job v6

--- .github/actions/noti_mailgun/index.js
+++ .github/actions/noti_mailgun/index.js
@@ -1,26 +1,27 @@
 const core = require("@actions/core");
 
-//Variables mailgun crear formulario
+// Variables mailgun crear formulario
 var API_KEY_MAILGUN = core.getInput('resultado_cypress');
 var DOMAIN_MAILGUN = core.getInput('resultado_cypress');
 let DESTINATARIO = process.env.destinatario
+
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({ username: 'api', key: API_KEY_MAILGUN });
+const form_mailgun = new Mailgun(formData);
+const send_message = form_mailgun.client({ username: 'api', key: API_KEY_MAILGUN });
 
 //Variables de estado de los jobs
-let result_linter_job = core.getInput('result_linter_job');
-let resultado_cypress = core.getInput('result_cypress_job');
-let result_add_badge_job = core.getInput('result_add_badge_job');
-let result_deploy_job = core.getInput('result_deploy_job');
+let result_linter_job = "success";
+let resultado_cypress = "success";
+let result_add_badge_job = "success";
+let result_deploy_job = "success";
 
 const asunto= "Resultado del workflow ejecutado"
 
 const body = `
     <div>
         <p>Se ha realizado un push en la rama master que ha provocado la ejecución del
-            workflow GithubActions_Final con los siguientes resultados:</p>
+            workflow vicent_GhActions_workflow con los siguientes resultados:</p>
         <ul>
             <li>
                 linter_job: ${result_linter_job}
@@ -37,7 +38,7 @@ const body = `
         </ul>
     </div>`;
 
-mg.messages.create(DOMAIN_MAILGUN, {
+send_message.messages.create(DOMAIN_MAILGUN, {
     from: "esteve.ferre.vicent@gmail.com",
     to: [DESTINATARIO],
     subject: asunto,
